62cb3bac0acbef3cde18d6973ea17ad355c8e403
Nishant Mishra <nishant@cori.nersc.gov>
2020-04-17 16:55:15 -0700

    first commit

---
+++ headers.py
@@ -0,0 +1,7 @@
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import scipy.ndimage as ndimage
+import scipy.stats as stats
+import numpy.random as random
